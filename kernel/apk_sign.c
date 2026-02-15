4bf88276300f11378cd2e1d4e5cd7d7972f22222
Sorayukii <sorayukii69@gmail.com>
2026-02-15 20:39:47 +0700

    kernel: Add signature check for RapliVx/KernelSU
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -401,5 +401,6 @@ bool is_manager_apk(char *path)
 	|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588")  // KOWX712/KernelSU
 	|| check_v2_signature(path, 0x3e6, "79e590113c4c4c0c222978e413a5faa801666957b1212a328e46c00c69821bf7")  // rifsxd/KernelSU-Next
 	|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b")  // rsuntk/KernelSU
+	|| check_v2_signature(path, 0x384, "a9462b8b98ea1ca7901b0cbdcebfaa35f0aa95e51b01d66e6b6d2c81b97746d8")  // RapliVx/KernelSU
 	);
 }
